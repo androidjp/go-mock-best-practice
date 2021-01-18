9e8aa12ae7c210b69f3690e4ea254d182b4d2c29
androidjp <1049578026@qq.com>
2021-01-18 22:49:20 +0800

    update master

--- 1_gomock/db/repository.go
+++
@@ -1,9 +0,0 @@
-package gomock_db
-
-type Repository interface {
-	Create(key string, value []byte) error
-	Retrieve(key string) ([]byte, error)
-	Update(key string, value []byte) error
-	Delete(key string) error
-}
-
