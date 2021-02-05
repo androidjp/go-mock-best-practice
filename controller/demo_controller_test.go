02b0adef9bca7a56a9feef85b1bcdf49ed40eaab
wujunpeng <wujunpeng@wps.cn>
2021-02-05 17:32:38 +0800

    feat: 新增原生http服务 接口mock例子

--- controller/demo_controller_test.go
+++ controller/demo_controller_test.go
@@ -1,11 +1,14 @@
 package controller_test
 
 import (
+	"bou.ke/monkey"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"go-mock-best-practice/controller"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -31,5 +34,57 @@ func TestDemoController_GetMessage(t *testing.T) {
 		//---------------------------------------
 		assert.Nil(t, err)
 		assert.Equal(t, "Hello Mike!", string(bodyBytes))
+	})
+
+	// 通过mock API接口，来达到“想响应啥，就响应啥”的目的
+	t.Run("should return `Hello Jasper` when mock the API", func(t *testing.T) {
+
+		//---------------------------------------
+		// given
+		//---------------------------------------
+		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, request *http.Request) {
+			fmt.Fprintf(w, "Hello Jasper")
+		}))
+		defer ts.Close()
+
+		//---------------------------------------
+		// when
+		//---------------------------------------
+		resp, err := http.Get(ts.URL)
+		defer resp.Body.Close()
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+		//---------------------------------------
+		// then
+		//---------------------------------------
+		assert.Nil(t, err)
+		assert.Equal(t, "Hello Jasper", string(bodyBytes))
+	})
+
+	// 或者通过monkey.PatchInstanceMethod 来mock controller层逻辑，来达到目录
+	t.Run("should return `Hello Jasper` when mock the controller layer logic", func(t *testing.T) {
+
+		//---------------------------------------
+		// given
+		//---------------------------------------
+		demoCtrl := &controller.DemoController{}
+		monkey.PatchInstanceMethod(reflect.TypeOf(demoCtrl), "GetMessage", func(ctrl *controller.DemoController, w http.ResponseWriter, request *http.Request) {
+			fmt.Fprintf(w, "Hello Jasper")
+		})
+		ts := httptest.NewServer(http.HandlerFunc(demoCtrl.GetMessage))
+		defer ts.Close()
+
+		//---------------------------------------
+		// when
+		//---------------------------------------
+		resp, err := http.Get(ts.URL)
+		defer resp.Body.Close()
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+		//---------------------------------------
+		// then
+		//---------------------------------------
+		assert.Nil(t, err)
+		assert.Equal(t, "Hello Jasper", string(bodyBytes))
 	})
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-mock-best-practice
 
 go 1.15
 
-require github.com/stretchr/testify v1.7.0
+require (
+	bou.ke/monkey v1.0.2
+	github.com/stretchr/testify v1.7.0
+)
