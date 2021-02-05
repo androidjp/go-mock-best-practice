ca75213058de062b492e3f976e3af2f62af679ae
wujunpeng <wujunpeng@wps.cn>
2021-02-05 14:50:06 +0800

    refactor: 整理不相关的代码

--- 1_gomock/adapter/json.go
+++ 1_gomock/adapter/json.go
@@ -4,5 +4,7 @@ import (
 	"encoding/json"
 )
 
+// 使用 adapter包来存放为了测试打桩而声明的变量
+// 指向第三方库函数的对象（为了打桩而使用）
 var Marshal = json.Marshal
 var UnMarshal = json.Unmarshal
--- 1_gomock/service/demo_test.go
+++ 1_gomock/service/demo_test.go
@@ -1,134 +1,11 @@
 package gomock_service_test
 
 import (
-	"errors"
-	"github.com/golang/mock/gomock"
 	"github.com/prashantv/gostub"
 	"github.com/stretchr/testify/assert"
 	gomock_service "go-mock-best-practice/1_gomock/service"
-	mock_gomock_db "go-mock-best-practice/test/1_gomock/db"
 	"testing"
 )
-
-func TestDemoService_InsertData(t *testing.T) {
-	t.Run("should_return_tips_and_err_given_InsertData_when_repo_return_mongo_connection_error", func(t *testing.T) {
-		// given
-		// 1. 初始化 mock控制器
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		// 2. 初始化mock对象，并注入控制器
-		mockRepo := mock_gomock_db.NewMockRepository(ctrl)
-
-		// 3. 设定mock对象的返回值
-		mockRepo.EXPECT().Create("name", []byte("jasper")).Return(errors.New("db connection error")).Times(3)
-		demoSvr := &gomock_service.DemoService{Repo: mockRepo}
-
-		// when
-		data, err := demoSvr.InsertData("name", "jasper")
-
-		// then
-		assert.Equal(t, "need to retry", data)
-		assert.NotNil(t, err)
-		assert.Equal(t, "db connection error", err.Error())
-	})
-
-	t.Run("should_return_empty_and_err_given_InsertData_when_repo_return_other_error", func(t *testing.T) {
-		// given
-		// 1. 初始化 mock控制器
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		// 2. 初始化mock对象，并注入控制器
-		mockRepo := mock_gomock_db.NewMockRepository(ctrl)
-
-		// 3. 设定mock对象的返回值
-		mockRepo.EXPECT().Create("name", []byte("jasper")).Return(errors.New("数据不存在")).Times(3)
-		demoSvr := &gomock_service.DemoService{Repo: mockRepo}
-
-		// when
-		data, err := demoSvr.InsertData("name", "jasper")
-
-		// then
-		assert.Equal(t, "", data)
-		assert.NotNil(t, err)
-		assert.Equal(t, "数据不存在", err.Error())
-	})
-
-	t.Run("should_return_success_given_InsertData_when_repo_return_nil", func(t *testing.T) {
-		// given
-		// 1. 初始化 mock控制器
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		// 2. 初始化mock对象，并注入控制器
-		mockRepo := mock_gomock_db.NewMockRepository(ctrl)
-
-		// 3. 设定mock对象的返回值
-		mockRepo.EXPECT().Create("name", []byte("jasper")).Return(nil)
-		demoSvr := &gomock_service.DemoService{Repo: mockRepo}
-
-		// when
-		data, err := demoSvr.InsertData("name", "jasper")
-
-		// then
-		assert.Equal(t, "success", data)
-		assert.Nil(t, err)
-	})
-
-	t.Run("should_return_success_given_InsertData_when_repo_return_nil_at_the_third_time", func(t *testing.T) {
-		// given
-		// 1. 初始化 mock控制器
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		// 2. 初始化mock对象，并注入控制器
-		mockRepo := mock_gomock_db.NewMockRepository(ctrl)
-
-		// 3. 设定mock对象的返回值
-		mockRepo.EXPECT().Create("name", []byte("jasper")).Return(errors.New("db connection error")).Times(2) // 前两次返回错误
-		mockRepo.EXPECT().Create("name", []byte("jasper")).Return(nil)                                        // 第三次正常
-
-		demoSvr := &gomock_service.DemoService{Repo: mockRepo}
-
-		// when
-		data, err := demoSvr.InsertData("name", "jasper")
-
-		// then
-		assert.Equal(t, "success", data)
-		assert.Nil(t, err)
-	})
-}
-
-func TestDemoService_CheckAndUpdateData(t *testing.T) {
-	t.Run("should_return_nil_given_can_find_existed_data", func(t *testing.T) {
-		// given
-		// 1. 初始化 mock控制器
-		ctrl := gomock.NewController(t)
-		defer ctrl.Finish()
-
-		// 2. 初始化mock对象，并注入控制器
-		mockRepo := mock_gomock_db.NewMockRepository(ctrl)
-
-		// 3. 设定mock对象的返回值
-		retrieveName := mockRepo.EXPECT().Retrieve("name").Return([]byte("jasper"), nil)
-		mockRepo.EXPECT().Update("name", []byte("mike")).Return(nil).After(retrieveName) // update 在 retrieve 之后
-
-		// 指定调用顺序的方式二
-		//gomock.InOrder(
-		//	mockRepo.EXPECT().Retrieve("name").Return([]byte("jasper"), nil),
-		//	mockRepo.EXPECT().Update("name", []byte("mike")).Return(nil),
-		//)
-
-		demoSvr := &gomock_service.DemoService{Repo: mockRepo}
-
-		// when
-		err := demoSvr.CheckAndUpdateData("name", "mike")
-
-		// then
-		assert.Nil(t, err)
-	})
-}
 
 func TestDemoService_CheckConnect(t *testing.T) {
 	t.Run("全局变量打桩", func(t *testing.T) {
@@ -146,10 +23,14 @@ func TestDemoService_CheckConnect(t *testing.T) {
 func TestExec(t *testing.T) {
 	t.Run("函数打桩", func(t *testing.T) {
 		// given
-		stubs := gostub.Stub(&gomock_service.Exec, func(cmd string, args ...string) (string, error) {
-			return "xxx-vethName100-yyy", nil
-		})
+		// 方式一：StubFunc
+		stubs := gostub.StubFunc(&gomock_service.Exec, "xxx-vethName100-yyy", nil)
 		defer stubs.Reset()
+		// 方式二：Stub
+		//stubs := gostub.Stub(&gomock_service.Exec, func(cmd string, args ...string) (string, error) {
+		//	return "xxx-vethName100-yyy", nil
+		//})
+		//defer stubs.Reset()
 
 		// when
 		result, err := gomock_service.Exec("hello")
@@ -163,7 +44,13 @@ func TestExec(t *testing.T) {
 func TestDestroyResource(t *testing.T) {
 	t.Run("过程打桩", func(t *testing.T) {
 		// given
+		// 方式一：StubFunc（当你想这个过程啥都不做时，可以这样）
 		stubs := gostub.StubFunc(&gomock_service.DestroyResource)
 		defer stubs.Reset()
+		// 方式二：Stub
+		//stubs := gostub.Stub(&gomock_service.DestroyResource, func() {
+		//	// do sth
+		//})
+		//defer stubs.Reset()
 	})
 }
