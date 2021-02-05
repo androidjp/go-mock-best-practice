ec63fc0177b3fe86bb971d9efc22dcafc14cb00b
wujunpeng <wujunpeng@wps.cn>
2021-02-05 12:00:35 +0800

    style: 添加相关注释

--- 1_gomock/db/repository.go
+++ 1_gomock/db/repository.go
@@ -1,9 +1,9 @@
 package gomock_db
 
+// Repository 接口，拥有CRUD方法
 type Repository interface {
 	Create(key string, value []byte) error
 	Retrieve(key string) ([]byte, error)
 	Update(key string, value []byte) error
 	Delete(key string) error
 }
-
--- 1_gomock/service/demo.go
+++ 1_gomock/service/demo.go
@@ -5,10 +5,12 @@ import (
 	gomock_db "go-mock-best-practice/1_gomock/db"
 )
 
+// DemoService service业务层对象，手持Repository接口，拥有DB CRUD能力
 type DemoService struct {
 	Repo gomock_db.Repository
 }
 
+// InsertData 插入某条 key-value
 func (d *DemoService) InsertData(key, val string) (string, error) {
 	var err error
 	for i := 0; i < 3; i++ {
@@ -27,11 +29,14 @@ func (d *DemoService) InsertData(key, val string) (string, error) {
 	return "success", nil
 }
 
+// CheckAndUpdateData 检索并更新某条记录
 func (d *DemoService) CheckAndUpdateData(key, val string) error {
+	// 1. 先调用 repo 接口 Retrieve方法
 	data, err := d.Repo.Retrieve(key)
 	if data == nil || len(data) == 0 {
 		return fmt.Errorf("can not find key %s", key)
 	}
+	// 2. 后调用 repo 接口 Update方法
 	err = d.Repo.Update(key, []byte(val))
 	return err
 }
